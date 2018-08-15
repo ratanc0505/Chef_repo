b19bfd27844de101ebabc558e7f36568bdd3dfb6
kothamaddisiva <35668112+kothamaddisiva@users.noreply.github.com>
2018-08-15 00:54:43 -0400

    Update knife.rb

--- knife.rb
+++ knife.rb
@@ -5,6 +5,6 @@ log_level                :info
 log_location             STDOUT
 node_name                "venkata"
 client_key               "#{current_dir}/venkata.pem"
-chef_server_url          "https://13.56.161.133/organizations/demo"
+chef_server_url          "https://13.57.195.153/organizations/demo"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 ssl_verify_mode    :verify_none
