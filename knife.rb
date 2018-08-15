10c760e464769ac01eb67a2b1824bef351f567e0
kothamaddisiva <siva.kothamaddi@gmail.com>
2018-08-15 00:48:15 -0400

    cookbook will get artifacts from S3 and will deploy in tomact server

---
+++ knife.rb
@@ -0,0 +1,10 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "venkata"
+client_key               "#{current_dir}/venkata.pem"
+chef_server_url          "https://13.56.161.133/organizations/demo"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+ssl_verify_mode    :verify_none
