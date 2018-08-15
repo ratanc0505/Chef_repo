97c6e23c2af53cfcfea23132c86032e618bade3f
kothamaddisiva <35668112+kothamaddisiva@users.noreply.github.com>
2018-08-15 00:57:54 -0400

    Update knife.rb

--- knife.rb
+++ knife.rb
@@ -4,7 +4,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "venkata"
-client_key               "#{current_dir}/venkata.pem"
+client_key               "/home/centos/venkata.pem"
 chef_server_url          "https://13.57.195.153/organizations/demo"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 ssl_verify_mode    :verify_none
